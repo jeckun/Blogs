d50f7073f0f5880aabd6478cd74b7827797b8eae
Eric <eric.honghu@gmail.com>
2019-03-16 13:28:26 +0800

    学习Django

--- student_house/student_sys/student_sys/urls.py
+++ student_house/student_sys/student_sys/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from student.views import index
+
 
 urlpatterns = [
     path('', index, name='index'),
