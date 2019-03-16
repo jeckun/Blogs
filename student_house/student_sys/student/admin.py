159ce991b62d40219a12add7540f125e547f5919
Eric <eric.honghu@gmail.com>
2019-03-16 13:29:40 +0800

    学习Django

--- student_house/student_sys/student/admin.py
+++ student_house/student_sys/student/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Student
 
-# Register your models here.
-
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'sex', 'profession', 'email', 'qq', 'phone',
