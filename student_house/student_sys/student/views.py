bedd1c5ddff59e8075706261b401a7133738852b
Eric <eric.honghu@gmail.com>
2019-03-16 13:26:55 +0800

    学习Django

---
+++ student_house/student_sys/student/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+
+
+def index(request):
+    # v1: 实现传递一个单词到index页面进行渲染
+    # words = 'World!'
+    # return render(request, 'index.html', context={'words': words, 'student_name': 'Eric'})
+
+    # v2: 实现传递一个对象列表到index页面进行渲染
+    students = Student.objects.all()
+    return render(request, 'index.html', context={'students': students})
