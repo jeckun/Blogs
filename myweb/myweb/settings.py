b4f23b6c88f7240c29f72bd41fd0e7fd463d4a99
Eric <eric.honghu@gmail.com>
2019-03-29 15:18:40 +0800

    创建MyWeb

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4n&yk!h9^mq7f3ocp2*h%2@qoc78-*n^28zn-du!!4s1*p1qzo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.208','10.122.10.3', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.43.208', '10.122.10.3', 'localhost', '127.0.0.1']
 
 
 # Application definition
