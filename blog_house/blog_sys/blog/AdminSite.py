a23cd68dc184f1bd39f3b57e5e91619fd7227002
Eric <eric.honghu@gmail.com>
2019-07-08 22:38:25 +0800

    完成第六章多管理入口更新。 localhost:8000/admin 编辑文章 localhost:8000/super_admin 进行用户及权限管理

--- blog_house/blog_sys/blog/AdminSite.py
+++ blog_house/blog_sys/blog/AdminSite.py
@@ -5,7 +5,7 @@ class CustomSite(AdminSite):
     # 自定义管理后台
     site_header = 'Blog'
     site_title = 'Blog 管理后台'
-    index_title = '首页'
+    index_title = '作者后台'
 
 
 custom_site = CustomSite(name='cus_admin')
