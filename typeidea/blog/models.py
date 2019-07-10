7ce141f2f9cb74618f8715bbfa7a85d7412233ee
Eric <eric.honghu@gmail.com>
2019-07-10 13:43:33 +0800

    第7章 博客浏览页面实现

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -94,6 +94,34 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    @staticmethod
+    def get_by_tag(tag_id):
+        # 根据标签获取文章列表
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category=None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        queryset = cls.objects.filter(status=Post.STATUS_NORMAL)
+        return queryset
+
     class Meta:
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']    # 根据id降序排列
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,17 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Tag, Category, Link
 
 
 def post_list(request, category_id=None, tag_id=None):
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+    """
+    用于展示文章列表，返回一组文章的名称和摘要
+    """
+    category = None
+    tag = None
+    # post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+    post_list = Post.latest_posts()
+
+    # if tag_id:
+    #     try:
+    #         tag = Tag.objects.get(id=tag_id)
+    #         # post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    #         post_list = tag.post_set.all()
+    #     except Tag.DoesNotExist:
+    #         post_list = []
+    #
+    # if category_id:
+    #     try:
+    #         category = Category.objects.get(id=category_id)
+    #         post_list = post_list.filter(category_id=category_id)
+    #     except Post.DoesNotExist:
+    #         post_list = []
+
+    if tag_id:
+        post_list, tag = Post.get_by_tag(tag_id)
+
+    if category_id:
+        post_list, category = Post.get_by_category(category_id)
+
+    context={
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+
+    return render(request, 'blog/list.html', context=context)
 
 
 # def post_detail(request, post_id):
 #     # 测试验证URL是否正确
 #     return HttpResponse('detail')
-
 def post_detail(request, post_id):
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+    """
+    显示文章内容明细
+    """
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
 
 
 def links(request):
